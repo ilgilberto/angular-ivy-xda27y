26ff70f705a75ede31b9985f597d50d5b700874c
ilgilberto <jil64@outlook.it>
2022-03-15 23:29:16 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+ngOnInit(): void {
+
+    this.carta1 = 'RD';
+    this.carta2 = 'RD';
+    this.carta3  = 'RD';
+
+    this.mazzo = 'CC';
+    this.briscola = 'RD';
+
+    this.giocata1 = 'CC';
+    this.giocata2 = 'CC';
+  
+}
+
+  carta1 :string;
+  carta2 :string
+  carta3: string;
+
+  mazzo: string;
+  briscola: string;
+
+  giocata1: string;
+  giocata2: string;
+
+
+
   name = 'Angular ' + VERSION.major;
 }
